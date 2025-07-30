aea9be4ec827e0cd03319d0024e56360b106409c
mykhatilo-vlad <mykhatilo.vlad@gmail.com>
2025-07-30 16:21:28 +0300

    add single item slider

---
+++ src/main.ts
@@ -0,0 +1,65 @@
+import './style.css';
+
+class ScrollSnapSlider extends HTMLElement {
+  itemsWrapper: HTMLElement | null = null;
+  items: HTMLCollection | null = null;
+  itemsLength: number = 0;
+  arrowsWrapper: HTMLElement | null = null;
+  arrowPrev: HTMLElement | null = null;
+  arrowNext: HTMLElement | null = null;
+
+  currentIndex: number = 0;
+
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    this.itemsWrapper = this.querySelector('[is="slider-items"]');
+    this.arrowsWrapper = this.querySelector('[is="slider-arrows"]');
+    this.arrowPrev = this.querySelector('[is="slider-prev"]');
+    this.arrowNext = this.querySelector('[is="slider-next"]');
+    
+    this.renderItems();
+
+    this.arrowPrev?.addEventListener('click', this.slidePrev);
+    this.arrowNext?.addEventListener('click', this.slideNext);
+  }
+
+  disconnectedCallback() {
+   
+  }
+
+  renderItems = () => {
+    if (this.itemsWrapper) {
+      const items = Array.from({ length: 10 }, (_, i) => {
+        return `<div class="slide-card ">Item ${i + 1}</div>`;
+      });
+
+      this.itemsWrapper.innerHTML = items.join('');
+      this.items = this.itemsWrapper.children;
+      this.itemsLength = this.items.length;
+    }
+  }
+
+  slidePrev = () => {
+    this.currentIndex = this.currentIndex > 0 ? --this.currentIndex : (this.itemsLength - 1);
+    this.slideTo(this.currentIndex);
+  }
+
+  slideNext = () => {
+    this.currentIndex = this.currentIndex < (this.itemsLength - 1) ? ++this.currentIndex : 0;
+    this.slideTo(this.currentIndex);
+  }
+  
+  slideTo = (index: number) => {
+    if (this.items) {
+      const item = this.items[index];
+      item?.scrollIntoView({ behavior: 'smooth' });
+    }
+  }
+}
+
+customElements.define('scroll-snap-slider', ScrollSnapSlider);
+
+
